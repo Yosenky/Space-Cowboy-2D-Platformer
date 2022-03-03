9ad30d72d959acf68d9b82179a9f0d5d0e4d1dd9
Yosenky <jackson.mccall907@gmail.com>
2022-03-03 12:15:21 -0600

    Added Spikes and started finalboss script

---
+++ Assets/Script/FinalBossBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossBehavior : MonoBehaviour
+{
+
+  public GameObject cactusBulletStage1; //Bullets shot out of cactus during stage 1
+
+
+
+    void Start()
+    {
+        stageOneShoot();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //Shooting script for stage 1
+    void stageOneShoot()
+    {
+      //Instantiate bullets around cactus
+      int numberOfBullets = Random.Range(5,8);
+      for(int i = 0; i < 1; i++){
+        Instantiate(cactusBulletStage1, new Vector3(transform.position.x, transform.position.y, 0),Quaternion.Euler(0, 0, 90 + (180/numberOfBullets)*i));
+      }
+      //Pause
+      //Shoot slowly out from spawn
+    }
+}
