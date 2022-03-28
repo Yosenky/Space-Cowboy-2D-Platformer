c7565e9c059e627227626ea30ff1931f9bd26323
nicksong661 <nicksong661@gmail.com>
2022-03-28 14:05:24 -0500

    player

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,49 +4,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   private Vector3 direction;
-   public float strength = 5f, velocity=5f;
+   public Rigidbody2D rb;
+   private float strength = 250f, velocity=5f;
    public float bulletSpeed = 10;
    public Rigidbody bullet;
+   private bool jumping=false;
 
-   private void Update(){
-     Jump();
+   void Awake(){
+    rb = GetComponent<Rigidbody2D>();
+   }
+   void Update(){
+     jump();
      move();
 
      if (Input.GetKeyDown(KeyCode.J) || Input.GetMouseButtonDown(0)){
              Fire();
      }
 
-     transform.position += direction*Time.deltaTime;
-
    }
 
-   private void Jump(){
-    if (Input.GetKeyDown(KeyCode.Space))
-        {
-            direction = Vector3.up * strength;
+     void jump(){
+        if(Input.GetKeyDown(KeyCode.Space)&&!jumping){
+          rb.AddForce(transform.up*strength);
+          jumping=true;
         }
-   } 
-
-   private void move(){
-    if (Input.GetKeyDown(KeyCode.A))
-        {
-            direction = Vector3.left * velocity;
-        }
-    if (Input.GetKeyDown(KeyCode.D))
-        {
-            direction = Vector3.right * velocity;
-        }
-   }  
-
-   
-     
-     
+     }
      void Fire()
      {
          Rigidbody bulletClone = (Rigidbody) Instantiate(bullet, transform.position, transform.rotation);
          bulletClone.velocity = new Vector2(bulletSpeed,0.0f);
          bulletClone.AddForce(bulletClone.velocity,(ForceMode.Force));
+     }
+
+     void move(){
+        if (Input.GetKeyDown (KeyCode.A)) 
+         {
+             rb.velocity = new Vector2 (velocity * -1,rb.velocity.y);
+         }
+        if (Input.GetKeyUp (KeyCode.A)) 
+        {
+            rb.velocity = new Vector2 (0,0);
+        }
+        if (Input.GetKeyDown (KeyCode.D)) 
+        {
+            rb.velocity = new Vector2 (velocity, rb.velocity.y);
+        }
+        if (Input.GetKeyUp (KeyCode.D)) 
+        {
+            rb.velocity = new Vector2 (0,0);
+        }
      }
  
 
