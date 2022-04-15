9912578eab94c8976f5e10f9d4db783494b883fa
cojohnson1999 <cojohnson1999@gmail.com>
2022-04-15 13:56:10 -0500

    Added camera following player, invisible walls for level bounds

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -6,36 +6,34 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform followTransform;
+    public BoxCollider2D leftMapBound, rightMapBound, topMapBound, bottomMapBound;
 
     private float xMin, xMax, yMin, yMax;
-    public BoxCollider2D mapBounds;
     private float camY,camX;
-    private float camOrthsize;
-    private float cameraRatio;
+    private float width;
+    private float height;
     private Camera mainCam;
     private Vector3 smoothPos;
     public float smoothSpeed = 0.5f;
 
     private void Start()
     {
-        xMin = -14.00411f; 
-        xMax = 40.01427f;
-        yMin = -7.98649f; 
-        yMax = 10.56783f;
-        Debug.Log("xMin = " + xMin + ", xMax = " + xMax + ", yMin = " + yMin + ", yMax = " + yMax);
+        xMin = leftMapBound.bounds.max.x;
+        xMax = rightMapBound.bounds.min.x;
+        yMin = bottomMapBound.bounds.max.y;
+        yMax = topMapBound.bounds.min.y;
         mainCam = GetComponent<Camera>();
-        camOrthsize = mainCam.orthographicSize;
-        cameraRatio = (xMax + camOrthsize) / 2.0f;
+        //Camera view height is 2 * the orthographic size
+        height = mainCam.orthographicSize * 2;
+        //Camera width is the aspect of the camera view * the height
+        width = height * mainCam.aspect;
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        camY = Mathf.Clamp(followTransform.position.y, yMin - camOrthsize, yMax + camOrthsize);
-        camX = Mathf.Clamp(followTransform.position.x, xMin - cameraRatio, xMax + cameraRatio);
-        Debug.Log("camX = " + camX + ", camY = " + camY);
+        camY = Mathf.Clamp(followTransform.position.y, yMin, yMax);
+        camX = Mathf.Clamp(followTransform.position.x, xMin, xMax);
         smoothPos = Vector3.Lerp(this.transform.position, new Vector3(camX, camY, this.transform.position.z), smoothSpeed);
         this.transform.position = smoothPos;
-        
-        
     }
 }
