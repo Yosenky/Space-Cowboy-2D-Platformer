b9fdd60473b2dec544e1a9efceac0a3f71803f5a
cojohnson1999 <cojohnson1999@gmail.com>
2022-04-21 12:05:08 -0500

    Created portal animation script

---
+++ Assets/Script/AnimatePortal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatePortal : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
+    void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void AnimateSprite(){
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length){
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+}
